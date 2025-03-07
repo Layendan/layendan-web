561cdd503c2df0f88bbf21c13ffb9c8dfd637dd0
Layendan <layendan.h@gmail.com>
2025-03-06 21:49:35 -0700

    Update main page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <svelte:head>
   <title>Layendan</title>
   <meta property="og:title" content="Layendan" />
-  <meta property="og:description" content="Please hire me 🙏" />
+  <meta property="og:description" content="Coding for fun" />
   <meta property="og:type" content="website" />
   <meta property="og:image" content="https://layendan.dev/imgs/home.png" />
   <meta property="og:image:alt" content="layendan.dev" />
@@ -21,31 +21,8 @@
   <meta name="twitter:creator" content="@layendan" />
 </svelte:head>
 
-<div class="grid h-screen w-screen place-items-center">
+<div class="-mt-16 grid h-screen w-screen place-items-center">
   <div class="flex flex-col gap-4 text-center">
     <h1 class="text-5xl font-bold">Layendan</h1>
-    <p>please hire me 🙏</p>
-    <a
-      href="https://github.com/Layendan"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="btn btn-link btn-lg"
-    >
-      <Fa icon={faGithub} />
-      Github
-    </a>
-    <a href="discord://-/users/235566430693752832" class="btn btn-link btn-lg">
-      <Fa icon={faDiscord} />
-      layendan
-    </a>
-    <a
-      href="https://www.linkedin.com/in/aidan-labourdette/"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="btn btn-link btn-lg"
-    >
-      <Fa icon={faLinkedin} />
-      LinkedIn
-    </a>
   </div>
 </div>
